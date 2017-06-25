e0f7839bd8c60585565aea29e4774700541c1055
James Bithell <hi@jbithell.com>
2017-06-25 19:55:59 +0100

    Remove test

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ app = Flask(__name__) #This is to keep Heroku Happy
 app.run(os.environ.get('PORT')) #This is to keep Heroku Happy
 print("WebServer online")
 
-while True:
-	print("Hi")
+def elec_trace():
+	thisusagepoint = le.electricity_useage
+	print("New Reading ", str(thisusagepoint))
+	webrequest = urllib.request.urlopen(str(os.environ['url']) + "?kwh=" + str(thisusagepoint))
+	if (webrequest.read() != "DONE"):
+		print("Error sending result")
+	 
+print("Starting monitoring")
+le = pyloopenergy.LoopEnergy(str(os.environ['serial']), str(os.environ['secret']))
+le.subscribe_elecricity(elec_trace) #Start listening for data
