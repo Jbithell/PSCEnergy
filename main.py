66778eea75162c01bb2e061c1b55889ce62637b4
James Bithell <hi@jbithell.com>
2017-07-20 17:11:19 +0100

    Add error handling

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ logging.basicConfig() #Handy Bug Fix - see https://github.com/pavoni/pyloopenerg
 import pyloopenergy  #pyloopenergy
 import os #Environment Variables
 import urllib.request #Web request
+from raven import Client #Logging
+ravenErrorClient = Client('https://45d5760c5f86474693af392908cd6915:5b846b53fa2544a8af6d58e095cc43e2@sentry.io/193950') #Setup error client
 
 
 print("Starting")
@@ -12,8 +14,8 @@ def elec_trace():
 	print("New Reading ", str(thisusagepoint))
 	webrequest = urllib.request.urlopen(str(os.environ['url']) + "?kwh=" + str(thisusagepoint))
 	if (str(webrequest.read().decode("utf-8")) != 'DONE'):
-		print("Error sending result")
-	 
+		ravenErrorClient.captureMessage("Error sending result to server " + str(thisusagepoint))
+
 print("Starting monitoring")
 le = pyloopenergy.LoopEnergy(str(os.environ['serial']), str(os.environ['secret']))
 le.subscribe_elecricity(elec_trace) #Start listening for data
